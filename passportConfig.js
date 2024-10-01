3921bce4bd4cd04f20770fc9af9fd7e3f60a6d16
Matt <makwong@dons.usfca.edu>
2024-10-01 14:33:47 -0700

    idk

--- passportConfig.js
+++ passportConfig.js
@@ -22,6 +22,8 @@ function initializePassport(app) {
         secret: 'SuperSecretSecret',
         resave: false,
         saveUninitialized: false,
+        proxy: true,
+        name: "PleaseWork",
         cookie: {
             secure: true, // This should be true if you're using HTTPS
             httpOnly: true, // Ensure cookie is only sent via HTTP(S), not client-side JavaScript
