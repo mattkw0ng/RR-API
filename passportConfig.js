42b4e388a1c5132f7cfe0f7e51b7260b58974c21
Matt <makwong@dons.usfca.edu>
2024-10-01 15:02:34 -0700

    partitioned?

--- passportConfig.js
+++ passportConfig.js
@@ -28,7 +28,8 @@ function initializePassport(app) {
             secure: true, // This should be true if you're using HTTPS
             httpOnly: true, // Ensure cookie is only sent via HTTP(S), not client-side JavaScript
             sameSite: 'none', // This is important for cross-origin requests
-            maxAge: 1000 * 60 * 60 * 24 // Set cookie expiration (optional, e.g., 24 hours)
+            maxAge: 1000 * 60 * 60 * 24, // Set cookie expiration (optional, e.g., 24 hours)
+            partitioned: true,
         }
     }));
     app.use(passport.initialize());
