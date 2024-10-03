12f308b7511bd9dff420e53c6e07195ed193cf9a
Matt <makwong@dons.usfca.edu>
2024-10-02 18:35:07 -0700

    headers

--- server.js
+++ server.js
@@ -45,6 +45,14 @@ app.use(session({
   }
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-HTTP-Method-Override, Set-Cookie, Cookie");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();  
+});
+
 initializePassport(app);
 const PORT = 5000;
 
