2b4f9e1aecdad486c3d6fefea72fd8504f55e2a4
Matt <makwong@dons.usfca.edu>
2024-10-30 15:28:16 -0700

    log

--- events.js
+++ events.js
@@ -146,8 +146,11 @@ router.get('/userEvents', async (req, res) => {
     const pendingEvents = getUserEvents(calendar, PENDING_APPROVAL_CALENDAR_ID, userEmail);
     const approvedEvents = getUserEvents(calendar, APPROVED_CALENDAR_ID, userEmail);
 
+    const result = {'pending': pendingEvents, 'approved': approvedEvents}
+    console.log(result)
+
     // console.log("++ /userEvents events:", events);
-    res.status(200).json({'pending': pendingEvents, 'approved': approvedEvents});
+    res.status(200).json(result);
   } catch (error) {
     res.status(500).send(error.message);
   }
