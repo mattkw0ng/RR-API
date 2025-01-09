9f7c08018e156d63ba5e9a7f11725190dd30562d
Matt <makwong@dons.usfca.edu>
2025-01-09 11:48:50 -0800

    moving to sjcac.org

--- config/config.js
+++ config/config.js
@@ -1,4 +1,6 @@
 const CLIENT_URL = "https://main--sensational-quokka-8e0ee2.netlify.app";
+
+const ALLOWED_CLIENT_URLS = ["https://main--sensational-quokka-8e0ee2.netlify.app", "https://rooms.sjcac.org"];
 // https://sjcactest.com
 
-module.exports = {CLIENT_URL};
+module.exports = {CLIENT_URL, ALLOWED_CLIENT_URLS};
