6c307119ee179cc328e3459db8e6077443413f3a
Matt <makwong@dons.usfca.edu>
2025-01-09 11:52:55 -0800

    rooms.sjcac

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-const CLIENT_URL = "https://main--sensational-quokka-8e0ee2.netlify.app";
+const CLIENT_URL = "https://rooms.sjcac.org";
 
 const ALLOWED_CLIENT_URLS = ["https://main--sensational-quokka-8e0ee2.netlify.app", "https://rooms.sjcac.org"];
 // https://sjcactest.com
